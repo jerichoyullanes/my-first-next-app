a73d93a05070c41c926b3202ed212f2ecbb3f01c
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-07 22:33:33 +0800

    Added useEffect samples

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,20 @@
 'use client'
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [user, setUser] = useState("Jericho");
+  const [user1, setUser1] = useState("Jericho1");
+  const [user2, setUser2] = useState("Jericho2");
 
-  const update = () => {
-    setUser("Rhona");
+  const update1 = () => {
+    setUser1("Rhona1");
   }
+
+  const update2 = () => {
+    setUser2("Rhona2");
+  }
+
+  useEffect(() => { console.log("useEffect Called") }, [user1]);
 
   // const color = (picker) => {
   //   alert(picker)
@@ -22,9 +29,10 @@ export default function Home() {
       <Page name="Fifth"/>
       <Page name="Sixth"/> */}
 
-      <h1>Hello my first name is {user}</h1>
+      <h1>Hello my first name is {user1} {user2}</h1>
       {/* <button onClick={() => color("Red") }>Click here</button> */}
-      <button onClick={() => update() }>Click here</button>
+      <button onClick={() => update1() }>Click here</button>
+      <button onClick={() => update2() }>Click here</button>
     </main>
   );
 }

a14ee215d0af4e43af52eba1bf56d0f32bb4c874
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-08 00:31:09 +0800

    Added Conditional Layout sample in blogs page

--- src/app/blogs/layout.js
+++ src/app/blogs/layout.js
@@ -1,17 +1,25 @@
+'use client'
 import Link from "next/link";
 import './style.css';
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }) {
+  const usePath = usePathname();
   return (
     <html lang="en">
       <body>
         <div>
-          <ul className="layout-menu">
-            <li><Link href="/blogs">Blog Page</Link></li>
-            <li><Link href="/blogs/Article">Articles</Link></li>
-            <li><Link href="/blogs/NewsBlog">News Blog</Link></li>
-            <li><Link href="/blogs/SportsBlog">Sports Blog</Link></li>
-          </ul>
+            {
+              usePath !== '/blogs/SportsBlog' ?
+                <ul className="layout-menu">
+                  <li><Link href="/blogs">Blog Page</Link></li>
+                  <li><Link href="/blogs/Article">Articles</Link></li>
+                  <li><Link href="/blogs/NewsBlog">News Blog</Link></li>
+                  <li><Link href="/blogs/SportsBlog">Sports Blog</Link></li>
+                </ul>
+              : null
+            }
+
         </div>
         {children}
       </body>
